ee890a5f884e9ce841252a65a056d6076a02f315
kennyzuo <353715843@qq.com>
2020-09-08 13:49:49 +0800

    ### 示例代码修改、美化

--- examples/demo/button-demo.vue
+++ examples/demo/button-demo.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <div class="row">
+  <div style="padding: 10px">
+    <div class="box">
       <p>Color</p>
       <xi-button color="info">Info</xi-button>
       <xi-button color="success">Success</xi-button>
@@ -11,7 +11,7 @@
       <xi-button light color="warning">Warning</xi-button>
       <xi-button light color="danger">Danger</xi-button>
     </div>
-    <div class="row">
+    <div class="box">
       <p>Sizes</p>
       <xi-button size="small">Small</xi-button>
       <xi-button size="Default">Default</xi-button>
@@ -19,24 +19,23 @@
       <xi-button size="medium">Medium</xi-button>
       <xi-button size="large">Large</xi-button>
     </div>
-    <div class="row">
+    <div class="box">
       <p>Full Width</p>
       <xi-button size="small" full color="success">Small</xi-button>
-      <xi-button size="Default" full color="success">Default</xi-button>
-      <xi-button size="normal" full color="success">Normal</xi-button>
-      <xi-button size="medium" full color="success">Medium</xi-button>
+      <xi-button size="Default" full color="warning">Default</xi-button>
+      <xi-button size="normal" full color="danger">Normal</xi-button>
+      <xi-button size="medium" full color="info">Medium</xi-button>
       <xi-button size="large" full color="success">Large</xi-button>
     </div>
 
-    <div class="row">
+    <div class="box">
       <p>Loading</p>
       <xi-button size="small" loading color="success">Small</xi-button>
-      <xi-button size="Default" loading color="success">Default</xi-button>
-      <xi-button size="normal" loading color="success">Normal</xi-button>
-      <xi-button size="medium" loading color="success">Medium</xi-button>
-      <xi-button size="large" loading color="success">Large</xi-button>
+      <xi-button size="Default" loading color="warning">Default</xi-button>
+      <xi-button size="normal" loading color="danger">Normal</xi-button>
+      <xi-button size="medium" loading color="info">Medium</xi-button> 
     </div>
-    <div class="row">
+    <div class="box">
       <p>Disabled</p>
       <xi-button size="small" disabled color="success">Small</xi-button>
       <xi-button size="Default" disabled color="success">Default</xi-button>
@@ -52,8 +51,12 @@ export default {}
 </script>
 
 <style lang="scss">
-.row {
-  padding: 10px 0;
+.box {
+  p {
+    margin-left: 10px;
+    font-size: 24px;
+    padding-bottom: 10px;
+  }
 }
 .button {
   margin-left: 10px;
